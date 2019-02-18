05523b0ea7fe707030ddbca9583bd2a586adf8f1
daneelgames <rubberandrobber@gmail.com>
2019-02-18 11:00:12 +0300

    enemy patrol behavior

--- unityProject/Assets/Scripts/EnemyController.cs
+++ unityProject/Assets/Scripts/EnemyController.cs
@@ -5,37 +5,88 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    public float normalSpeed = 3;
+    public float runSpeed = 3;
+
     public enum Behavior {Wait, Patrol, Wander, Chase};
     public Behavior behavior = Behavior.Wait;
     public HealthController healthController;
     public NavMeshAgent agent;
 
-    public List<Transform> wps = new List<Transform>();
+    public List<Vector3> waypoints = new List<Vector3>();
+    int nextWp = 0;
 
     ActionAreaController actionAreaController;
 
     private void Start()
     {
         healthController.enemy = this;
-        Invoke("Move1", 0);
+        ChooseBehavior();
     }
 
-    void Move1()
+    private void Update()
+    {
+        if (behavior == Behavior.Patrol)
+        { 
+            if (agent.enabled && !agent.pathPending && agent.remainingDistance < 0.5f)
+                GotoNextPoint();
+        }
+    }
+
+    void ChooseBehavior()
+    {
+        switch (behavior)
+        {
+            case Behavior.Wait:
+                agent.speed = 0;
+                break;
+
+            case Behavior.Wander:
+                agent.speed = normalSpeed;
+                Invoke("Wander", 0);
+                break;
+
+            case Behavior.Patrol:
+                agent.speed = normalSpeed;
+                agent.autoBraking = false;
+                GotoNextPoint();
+                break;
+        }
+    }
+
+    void GotoNextPoint()
+    {
+        if (waypoints.Count == 0)
+            return;
+
+        agent.destination = waypoints[nextWp];
+        nextWp = (nextWp + 1) % waypoints.Count;
+    }
+
+    void Wander()
     {
         Vector2 newPos = new Vector2(Random.Range(-18f, 18), Random.Range(-9f, 9f));
         agent.SetDestination(newPos);
-        Invoke("Move2", 0.5f);
+        Invoke("Wander", Random.Range(1, 5));
     }
 
-    void Move2()
-    {
-        Vector2 newPos = new Vector2(Random.Range(-18f, 18), Random.Range(-9f, 9f));
-        agent.SetDestination(newPos);
-        Invoke("Move1", 0.5f);
-    }
 
     public void SetActionAreaController(ActionAreaController area)
     {
         actionAreaController = area;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+
+        if (waypoints.Count > 0)
+        {
+            foreach (Vector3 t in waypoints)
+            {
+                if (t != null)
+                    Gizmos.DrawCube(t, Vector3.one / 2);
+            }
+        }
     }
 }
